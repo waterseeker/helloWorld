a03c98b31d91b129d530727680a9afaefeeee23b
Wayne Burris <waterseeker@hotmail.com>
2017-06-22 16:28:11 -0500

    hello world project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This Is My Hello World Project!");
+        }
+    }
+}
